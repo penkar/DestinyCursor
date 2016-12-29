24a52062e764d519c7032b70561c3c0ce17c65b1
Jeffrey Penkar <jspenkar@gmail.com>
2016-12-29 14:46:38 -0600

    Pakcage, webpack, and JS infrastructure added.

---
+++ js/index.js
@@ -0,0 +1,9 @@
+'use strict'
+import React from 'react'
+import { render } from 'react-dom';
+import Main from './components/Main'
+
+render(
+  <Main />,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// require('babel-polyfill');
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: './js/index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.OldWatchingPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'stage-0', 'react']
+        }
+      }
+    ]
+  },
+  watch: true
+};
