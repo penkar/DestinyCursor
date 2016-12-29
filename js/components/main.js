a9e16ffb345eefda0c8cb3b71139b775dec6b6cb
Jeffrey Penkar <jspenkar@gmail.com>
2016-12-29 15:18:41 -0600

    Check in, css changed to scss. loaders added.

--- js/components/main.js
+++ js/components/main.js
@@ -1,10 +1,35 @@
 import React, {Component, PropTypes} from 'react'
+require('../style/main.scss')
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clientY: 100,
+      clientX: 100,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('mousemove', ::this._mouseMove);
+  }
+
+  _mouseMove(data) {
+    let {clientY, clientX} = data;
+    this.setState({clientY, clientX});
+  }
 
   render() {
+    let {clientY, clientX} = this.state;
+    let style={top:clientY, left:clientX};
     return (
-      <div id='cursor'>
+      <div id='cursor' className='shared' style={style}>
+        <div className='shared child' style={style}>
+          <div className='shared' style={style}>
+            <div className='shared' style={style}>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,11 @@ module.exports = {
   module: {
     loaders: [
       {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+        exclude: /node_modules/
+      },
+      {
         test: /.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
