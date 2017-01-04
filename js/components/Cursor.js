e308ecccbca43209902c62ca1adef892766e283a
Jeffrey Penkar <jspenkar@gmail.com>
2017-01-03 21:22:09 -0600

    On click background changes for red, green and clear.

--- js/components/Cursor.js
+++ js/components/Cursor.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import cn from 'classnames';
 require('../style/Cursor.scss');
 
 export default class Cursor extends Component {
@@ -7,11 +8,22 @@ export default class Cursor extends Component {
     this.state = {
       clientY: 100,
       clientX: 100,
+      clicked: false,
     }
   }
 
   componentDidMount() {
     window.addEventListener('mousemove', ::this._mouseMove);
+    window.addEventListener('mousedown', ::this._mouseDown);
+    window.addEventListener('mouseup', ::this._mouseUp);
+  }
+
+  _mouseDown() {
+    this.setState({clicked: true});
+  }
+
+  _mouseUp() {
+    this.setState({clicked: false});
   }
 
   _mouseMove(data) {
@@ -20,11 +32,10 @@ export default class Cursor extends Component {
   }
 
   render() {
-    let {clientY, clientX} = this.state;
+    let {clientY, clientX, clicked} = this.state;
     let style={top:clientY-28, left:clientX-28};
-
     return (
-      <div id='cursor' style={style}>
+      <div id='cursor' style={style} className={cn({clicked})}>
         <ul>
           <li/>
           <li/>
