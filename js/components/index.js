2cee1b80c7b66e6f9b69e12c0cfd673dc02cab25
jsp <jspenkar@gmail.com>
2017-01-14 23:09:08 -0600

    source files removed from branch

--- js/components/index.js
+++
@@ -1,7 +0,0 @@
-import Body from './Body';
-import Cursor from './Cursor';
-import GenericBG from './GenericBG';
-import Header from './Header';
-import Term from './Term';
-
-export {Body, Cursor, GenericBG, Header, Term};
